aa90f5daf68aae249c4793982230ddbb4557dfe1
jrhoads <jrhoads@datacite.org>
2024-01-16 09:39:23 -0500

    First draft local gateway

---
+++ index.js
@@ -0,0 +1,36 @@
+import { ApolloServer } from 'apollo-server-micro';
+import { ApolloGateway } from '@apollo/gateway';
+import Cors from 'micro-cors-multiple-allow-origin';
+
+const cors = Cors(
+  {
+    origin:["https://studio.apollographql.com", "https://hoppscotch.io"],
+    allowMethods:["GET","POST"],
+    allowHeaders:["Access-Control-Allow-Credentials","true","Content-Type","Access-Control-Allow-Origin","Access-Control-Allow-Headers"]
+  }
+);
+const gateway = new ApolloGateway({
+  serviceList: [
+    { name: 'client-api', url: 'http://localhost:8065/client-api/graphql'},
+  ],
+});
+
+const server = new ApolloServer({
+  gateway});
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const startServer = server.start();
+
+export default cors(async function handler(req, res) {
+  await startServer;
+  if(req.method==="OPTIONS"){
+    res.end()
+    return false
+  }
+  await server.createHandler({ path: "/api/graphql" })(req, res);
+});
